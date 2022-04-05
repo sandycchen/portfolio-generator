723378dd82dd4eb9614037cc13b8345cf12d562b
Sandy <sandycchen30@gmail.com>
2022-04-05 14:15:42 -0700

    dynamically created HTML sections

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+const fs = require('fs');
+
+const generatePage = require('./src/page-template.js');
+
 const inquirer = require('inquirer');
 
 const promptUser = () => {
@@ -132,17 +136,11 @@ Add a New Project
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
+        const pageHTML = generatePage(portfolioData);
+
+        fs.writeFile('./index.html', pageHTML, err => {
+            if (err) throw new Error(err);
+
+            console.log('Portfolio complete! Check out index.html to see the output!');
+        });
     });
-
-// const fs = require('fs');
-
-// const generatePage = require('./src/page-template.js');
-
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile('./index.html', pageHTML, err => {
-//     if (err) throw new Error(err);
-
-//     console.log('Portfolio complete! Check out index.html to see the output!');
-// });
